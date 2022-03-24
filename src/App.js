c85302cab0fdf2a2fce434d5f06d0d558e4a84a0
John Pittenger <36045366+Gittenger@users.noreply.github.com>
2022-03-24 18:46:42 -0400

    mv page struct

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import HomePage from './pages/Home/Home.page.jsx'
+import HomePage from './pages/Home.page.jsx'
 
 function App() {
 	return <HomePage />
--- src/pages/Home.page.jsx
+++ src/pages/Home.page.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 
-import CIndex from '../../components/components.index.js'
+import CIndex from '../components/components.index.js'
+import styles from './_styles/Pages.module.scss'
 
 const HomePage = () => {
 	const { Directory } = CIndex
 
 	return (
-		<div>
+		<div className={styles.Home}>
 			<Directory />
 		</div>
 	)
