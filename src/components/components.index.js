645adf67195cf5c7a3f83461c7961b27c048bbbf
John Pittenger <36045366+Gittenger@users.noreply.github.com>
2022-03-24 18:28:56 -0400

    basic component setup

---
+++ src/components/components.index.js
@@ -0,0 +1,7 @@
+import Directory from './Directory/Directory.component.jsx'
+
+export const CIndex = {
+	Directory,
+}
+
+export default CIndex
---
+++ src/pages/Home/Home.page.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import CIndex from '../../components/components.index.js'
+
+const HomePage = () => {
+	const { Directory } = CIndex
+
+	return (
+		<div>
+			<Directory />
+		</div>
+	)
+}
+
+export default HomePage
