f6e720ff78b1cdb95093ba76ec0fe81fa4bcb5e1
John Pittenger <36045366+Gittenger@users.noreply.github.com>
2022-03-25 14:03:44 -0400

    implement styles & collectionItem comp

--- src/pages/CollectionDetails.page.jsx
+++ src/pages/CollectionDetails.page.jsx
@@ -1,9 +1,46 @@
 import React from 'react'
+import { useParams } from 'react-router-dom'
 
-const CollectionDetails = () => (
-	<div>
-		<h1>Collection Details</h1>
-	</div>
-)
+import CIndex from '../components/components.index'
+import styles from './_styles/Pages.module.scss'
+
+const CollectionDetails = props => {
+	const collection = {
+		title: 'Collection Title',
+		items: [
+			{
+				id: 1,
+				name: 'Starry Night',
+				imgSrc: 'https://m.media-amazon.com/images/I/71ciV1vrRPL._AC_SX679_.jpg',
+				price: '$170',
+			},
+			{
+				id: 2,
+				name: 'Dotted Rainbow',
+				imgSrc: 'https://m.media-amazon.com/images/I/81nRjwiG0ZL._AC_SX679_.jpg',
+				price: '$99',
+			},
+		],
+	}
+
+	// get title and items to map from collection
+	// get collection from state, fetch from firebase eventually
+	// use useSelector hook to select from collection state later
+	const { collectionName: title } = useParams()
+
+	const { CollectionItem } = CIndex
+	const { items } = collection
+
+	return (
+		<div className={styles.CollectionDetails}>
+			<h1>{title}</h1>
+			<div>
+				{items.map(item => (
+					<CollectionItem key={item.id} item={item} />
+				))}
+			</div>
+		</div>
+	)
+}
 
 export default CollectionDetails
