50de01b67562d520da618f9b5efac623f33e2aaa
John Pittenger <36045366+Gittenger@users.noreply.github.com>
2022-03-26 05:49:35 -0400

    configure store for sagas

--- src/redux/store.js
+++ src/redux/store.js
@@ -4,11 +4,22 @@ import cartSlice from './slices/cart.slice'
 import shopSlice from './slices/shop.slice'
 import userSlice from './slices/user.slice'
 
-export default configureStore({
-	reducer: {
-		directory: directorySlice,
-		cart: cartSlice,
-		shop: shopSlice,
-		user: userSlice,
-	},
-})
+import createSagaMiddleware from '@redux-saga/core'
+import rootSaga from './sagas/_root.saga'
+
+const sagaMiddleware = createSagaMiddleware()
+
+export default function store() {
+	const store = configureStore({
+		reducer: {
+			directory: directorySlice,
+			cart: cartSlice,
+			shop: shopSlice,
+			user: userSlice,
+		},
+		middleware: getDefaultMiddleware =>
+			getDefaultMiddleware({ thunk: false }).concat(sagaMiddleware),
+	})
+	sagaMiddleware.run(rootSaga)
+	return store
+}
