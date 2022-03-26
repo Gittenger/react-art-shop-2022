7970d8cc0c4da7c4161faebb9a588ec0d8506bc9
John Pittenger <36045366+Gittenger@users.noreply.github.com>
2022-03-26 14:30:41 -0400

    collection preview setup

--- src/components/components.index.js
+++ src/components/components.index.js
@@ -1,12 +1,14 @@
 import Directory from './Directory/Directory.component.jsx'
 import MenuItem from './MenuItem/MenuItem.component.jsx'
 import CollectionItem from './CollectionItem/CollectionItem.component.jsx'
+import CollectionPreview from './CollectionPreview/CollectionPreview.component.jsx'
 import Header from './Header/Header.component.jsx'
 
 export const CIndex = {
 	Directory,
 	CollectionItem,
 	MenuItem,
+	CollectionPreview,
 	Header,
 }
 
