82e83d0d32725f74893a1b3665c1054bef759590
John Pittenger <36045366+Gittenger@users.noreply.github.com>
2022-03-26 14:29:56 -0400

    basic header creation

--- src/components/components.index.js
+++ src/components/components.index.js
@@ -1,11 +1,13 @@
 import Directory from './Directory/Directory.component.jsx'
 import MenuItem from './MenuItem/MenuItem.component.jsx'
 import CollectionItem from './CollectionItem/CollectionItem.component.jsx'
+import Header from './Header/Header.component.jsx'
 
 export const CIndex = {
 	Directory,
 	CollectionItem,
 	MenuItem,
+	Header,
 }
 
 export default CIndex
