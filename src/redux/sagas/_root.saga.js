c60011a35b6f6c71bdb224fe39717bdbb2a73cb1
John Pittenger <36045366+Gittenger@users.noreply.github.com>
2022-03-26 05:48:50 -0400

    basic prep

---
+++ src/redux/sagas/_root.saga.js
@@ -0,0 +1,8 @@
+import { all, call } from 'redux-saga/effects'
+import cartSagas from './cart.sagas'
+import shopSagas from './shop.sagas'
+import userSagas from './user.sagas'
+
+export default function* rootSaga() {
+	yield all([call(cartSagas), call(shopSagas), call(userSagas)])
+}
