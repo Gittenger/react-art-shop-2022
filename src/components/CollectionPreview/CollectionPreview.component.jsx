93120c7f39c72d08cd8b7c01f0bde57d2c8731ed
John Pittenger <36045366+Gittenger@users.noreply.github.com>
2022-03-27 03:47:32 -0400

    collection preview base styles

--- src/components/CollectionPreview/CollectionPreview.component.jsx
+++ src/components/CollectionPreview/CollectionPreview.component.jsx
@@ -2,11 +2,13 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import CIndex from '../components.index'
 
+import styles from './CollectionPreview.module.scss'
+
 const CollectionPreview = ({ title, items }) => {
 	const { CollectionItem } = CIndex
 
 	return (
-		<div>
+		<div className={styles.Container}>
 			<h1>{title.toUpperCase()}</h1>
 			<Link to={`/shop/${title}`}>See more</Link>
 			<div>
