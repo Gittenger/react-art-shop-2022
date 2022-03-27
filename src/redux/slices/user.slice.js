dec85e6ab43e5463c963d90ed4993dbc92c9ea98
John Pittenger <36045366+Gittenger@users.noreply.github.com>
2022-03-27 14:18:43 -0400

    actions for sagas

--- src/redux/slices/user.slice.js
+++ src/redux/slices/user.slice.js
@@ -14,9 +14,30 @@ export const userSlice = createSlice({
 		}),
 		signOutSuccess: state => ({ ...state, currentUser: null, error: null }),
 		authFailure: (state, action) => ({ ...state, error: action.payload }),
+
+		// saga actions
+		// payload
+		signUpStart: (state, action) => state,
+		signUpSuccess: (state, action) => state,
+		emailSignInStart: (state, action) => state,
+
+		// no payload
+		googleSignInStart: state => state,
+		checkUserAuth: state => state,
+		signOutStart: state => state,
 	},
 })
 
-export const { signInSuccess, signOutSuccess, authFailure } = userSlice.actions
+export const {
+	signInSuccess,
+	signOutSuccess,
+	authFailure,
+	signUpStart,
+	signUpSuccess,
+	emailSignInStart,
+	googleSignInStart,
+	checkUserAuth,
+	signOutStart,
+} = userSlice.actions
 
 export default userSlice.reducer
