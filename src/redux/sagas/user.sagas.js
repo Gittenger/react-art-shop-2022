c8879f5f1fb26638f3a1dd392578a375d08058a4
John Pittenger <36045366+Gittenger@users.noreply.github.com>
2022-03-27 14:19:38 -0400

    outline for sagas started

--- src/redux/sagas/user.sagas.js
+++ src/redux/sagas/user.sagas.js
@@ -1,7 +1,37 @@
-import {all, call} from 'redux-saga/effects'
+import { all, put, call, takeLatest } from 'redux-saga/effects'
+import { userSlice } from '../slices/user.slice.js'
+
+const { actions } = userSlice
+
+export function* getSnapshotFromUser(user, otherData) {
+	try {
+		let createUserProfileDoc = function () {}
+		// createUserProfileDoc from firebase utils
+
+		let userRef = yield call(createUserProfileDoc, user, otherData)
+		let snapshot = yield userRef.get()
+
+		yield put(actions.signInSuccess({ id: snapshot.id, ...snapshot.data() }))
+	} catch (error) {
+		yield put(actions.authFailure(error.message))
+	}
+}
+
+export function* signInWithEmail({ payload: { email, password } }) {
+	try {
+		// auth module import from firebase utils, return obj with user
+		let auth
+		const { user } = yield auth.signInWithEmail(email, password)
+		yield getSnapshotFromUser(user)
+	} catch (error) {
+		yield put(actions.authFailure(error.message))
+	}
+}
+
+export function* onEmailSignInStart() {
+	yield takeLatest(actions.emailSignInStart, signInWithEmail)
+}
 
 export default function* userSagas() {
-	yield all([
-		// call each saga that is triggered by external action
-	])
+	yield all([call(onEmailSignInStart)])
 }
