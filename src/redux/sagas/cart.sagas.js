733512ae473f586fea7406786b97c468518d12b3
John Pittenger <36045366+Gittenger@users.noreply.github.com>
2022-03-28 02:45:46 -0400

    cart sagas OK

--- src/redux/sagas/cart.sagas.js
+++ src/redux/sagas/cart.sagas.js
@@ -1,7 +1,21 @@
-import { all, call } from 'redux-saga/effects'
+import { all, call, put, takeLatest } from 'redux-saga/effects'
+import { cartSlice } from '../slices/cart.slice.js'
+import { userSlice } from '../slices/user.slice.js'
+
+const { actions } = cartSlice
+
+export function* clearCartItems() {
+	yield put(actions.clearCart())
+}
+
+export function* onSignOutSuccess() {
+	yield takeLatest(userSlice.actions.signOutSuccess, clearCartItems)
+}
+
+export function* onCheckoutSuccess() {
+	yield takeLatest(actions.checkoutSuccess, clearCartItems)
+}
 
 export default function* cartSagas() {
-	yield all([
-		// call each saga that is triggered by external action
-	])
+	yield all([call(onSignOutSuccess), call(onCheckoutSuccess)])
 }
