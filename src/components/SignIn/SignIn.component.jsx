7cc81e319a2e2654c43f75fe83a80215e482fc0b
John Pittenger <36045366+Gittenger@users.noreply.github.com>
2022-03-29 15:03:26 -0400

    sign in dynamic inputs ok

--- src/components/SignIn/SignIn.component.jsx
+++ src/components/SignIn/SignIn.component.jsx
@@ -1,32 +1,52 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import styles from './SignIn.module.scss'
 
-const SignIn = () => (
-	<div>
-		<h2>Already have an account?</h2>
-		<span>Sign in with your email and password</span>
-		<form onSubmit={() => {}}>
-			<input
-				type="email"
-				name="email"
-				label="email"
-				value="email-state"
-				required
-			/>
-			<input
-				type="password"
-				name="password"
-				label="password"
-				value="password-state"
-				required
-			/>
-			<div>
-				<button type="submit">Sign in</button>
-				<button>Sign in with Google</button>
-			</div>
-		</form>
-	</div>
-)
+const SignIn = () => {
+	const [credentials, setCredentials] = useState({
+		email: '',
+		password: '',
+	})
+
+	const { email, password } = credentials
+
+	const handleChange = e => {
+		const { name, value } = e.target
+
+		setCredentials({
+			...credentials,
+			[name]: value,
+		})
+	}
+
+	return (
+		<div>
+			<h2>Already have an account?</h2>
+			<span>Sign in with your email and password</span>
+			<form onSubmit={() => {}}>
+				<input
+					onChange={handleChange}
+					type="email"
+					name="email"
+					label="email"
+					value={email}
+					required
+				/>
+				<input
+					onChange={handleChange}
+					type="password"
+					name="password"
+					label="password"
+					value={password}
+					required
+				/>
+				<div>
+					<button type="submit">Sign in</button>
+					<button>Sign in with Google</button>
+				</div>
+			</form>
+		</div>
+	)
+}
 
 export default SignIn
