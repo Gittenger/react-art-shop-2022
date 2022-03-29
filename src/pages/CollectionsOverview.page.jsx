348154e75755a954def4e6a5637f25e89effa40d
John Pittenger <36045366+Gittenger@users.noreply.github.com>
2022-03-29 18:03:57 -0400

    quick styles for overview

--- src/pages/CollectionsOverview.page.jsx
+++ src/pages/CollectionsOverview.page.jsx
@@ -1,15 +1,17 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
 import { selectCollectionsForPreview } from '../redux/selectors/shop.selectors'
+
 import CIndex from '../components/components.index'
+import styles from './_styles/Pages.module.scss'
 
 const CollectionsOverview = () => {
 	const { CollectionPreview } = CIndex
 	const collections = useSelector(selectCollectionsForPreview)
 
 	return (
-		<div>
-			<h1>Collections Overview</h1>
+		<div className={styles.ShopOverview}>
+			<h1>Browse our shop...</h1>
 			{collections.map(({ ...collectionProps }, i) => (
 				<CollectionPreview key={i} {...collectionProps} />
 			))}
