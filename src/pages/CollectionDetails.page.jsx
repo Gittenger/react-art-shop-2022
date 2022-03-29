a4ab878b28cfb54a0915ada884051e4988215923
John Pittenger <36045366+Gittenger@users.noreply.github.com>
2022-03-29 17:15:49 -0400

    implement shop selectors

--- src/pages/CollectionDetails.page.jsx
+++ src/pages/CollectionDetails.page.jsx
@@ -1,32 +1,16 @@
 import React from 'react'
 import { useParams } from 'react-router-dom'
+import { useSelector } from 'react-redux'
+import { selectShopCollection } from '../redux/selectors/shop.selectors'
 
 import CIndex from '../components/components.index'
 import styles from './_styles/Pages.module.scss'
 
 const CollectionDetails = props => {
-	const collection = {
-		title: 'Collection Title',
-		items: [
-			{
-				id: 1,
-				name: 'Starry Night',
-				imgSrc: 'https://m.media-amazon.com/images/I/71ciV1vrRPL._AC_SX679_.jpg',
-				price: '$170',
-			},
-			{
-				id: 2,
-				name: 'Dotted Rainbow',
-				imgSrc: 'https://m.media-amazon.com/images/I/81nRjwiG0ZL._AC_SX679_.jpg',
-				price: '$99',
-			},
-		],
-	}
-
-	// get title and items to map from collection
 	// get collection from state, fetch from firebase eventually
-	// use useSelector hook to select from collection state later
+	// rename collectionName urlParam to title, use with higher order selector
 	const { collectionName: title } = useParams()
+	const collection = useSelector(selectShopCollection(title))
 
 	const { CollectionItem } = CIndex
 	const { items } = collection
