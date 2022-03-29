0a43a7b1c8672905dacdca53a8caadff5a7b1247
John Pittenger <36045366+Gittenger@users.noreply.github.com>
2022-03-29 18:03:16 -0400

    directory selectors/data

---
+++ src/redux/directory.data.js
@@ -0,0 +1,22 @@
+export const sections = {
+	paintings: {
+		imgSrc:
+			'https://modpodgerocksblog.com/wp-content/uploads/2021/02/how-to-paint-clouds-735x955.jpg',
+		title: 'paintings',
+		linkUrl: '/shop/paintings',
+	},
+	sculptures: {
+		imgSrc:
+			'https://preview.redd.it/dxem0kif48p81.jpg?width=640&crop=smart&auto=webp&s=ab4cc6387f12bdaa88cf52f032cd732d2353480f',
+		title: 'sculptures',
+		linkUrl: '/shop/sculptures',
+	},
+	accessories: {
+		imgSrc:
+			'https://i.pinimg.com/736x/0e/42/3a/0e423aadbe35b815abf5d0de726b3f3a.jpg',
+		title: 'accessories',
+		linkUrl: '/shop/accessories',
+	},
+}
+
+export default sections
