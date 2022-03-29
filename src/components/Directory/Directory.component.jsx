f493df4a45260f3a2f055f81a1c8f6b2c5312d10
John Pittenger <36045366+Gittenger@users.noreply.github.com>
2022-03-29 18:03:41 -0400

    implement selector

--- src/components/Directory/Directory.component.jsx
+++ src/components/Directory/Directory.component.jsx
@@ -1,30 +1,19 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
+import { selectDirectorySections } from '../../redux/selectors/directory.selectors.js'
+
 import CIndex from '../components.index.js'
 import styles from './Directory.module.scss'
 
 const Directory = () => {
 	const { MenuItem } = CIndex
+	const sections = useSelector(selectDirectorySections)
 
 	return (
 		<div className={styles.Container}>
-			<MenuItem
-				imgSrc="https://modpodgerocksblog.com/wp-content/uploads/2021/02/how-to-paint-clouds-735x955.jpg"
-				subtitle="SHOP"
-				title="paintings"
-				linkUrl="/shop/paintings"
-			/>
-			<MenuItem
-				imgSrc="https://preview.redd.it/dxem0kif48p81.jpg?width=640&crop=smart&auto=webp&s=ab4cc6387f12bdaa88cf52f032cd732d2353480f"
-				subtitle="SHOP"
-				title="sculptures"
-				linkUrl="/shop/sculptures"
-			/>
-			<MenuItem
-				imgSrc="https://i.pinimg.com/736x/0e/42/3a/0e423aadbe35b815abf5d0de726b3f3a.jpg"
-				subtitle="SHOP"
-				title="accessories"
-				linkUrl="/shop/accessories"
-			/>
+			{sections.map(({ ...sectionProps }, i, arr) => {
+				return <MenuItem subtitle="SHOP" key={i} {...sectionProps} />
+			})}
 		</div>
 	)
 }
