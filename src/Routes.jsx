91e52f5c91da3a536df2fb6c38ed2f6e8288f910
John Pittenger <36045366+Gittenger@users.noreply.github.com>
2022-03-30 20:05:20 -0400

    using observer for current user

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -1,5 +1,7 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { useDispatch } from 'react-redux'
+import { checkUserAuth } from './redux/slices/user.slice.js'
 
 import Layout from './components/Layout/Layout.component.jsx'
 import HomePage from './pages/Home.page.jsx'
@@ -7,23 +9,31 @@ import Login from './pages/Login.page.jsx'
 import CollectionsOverview from './pages/CollectionsOverview.page.jsx'
 import CollectionDetails from './pages/CollectionDetails.page.jsx'
 
-const AppRoutes = () => (
-	<BrowserRouter>
-		<Layout>
-			<Routes>
-				<Route path="/">
-					<Route index element={<HomePage />} />
-					<Route path="shop">
-						<Route index element={<CollectionsOverview />} />
-						<Route path=":collectionName" element={<CollectionDetails />} />
+const AppRoutes = () => {
+	const dispatch = useDispatch()
+
+	useEffect(() => {
+		dispatch(checkUserAuth())
+	}, [dispatch])
+
+	return (
+		<BrowserRouter>
+			<Layout>
+				<Routes>
+					<Route path="/">
+						<Route index element={<HomePage />} />
+						<Route path="shop">
+							<Route index element={<CollectionsOverview />} />
+							<Route path=":collectionName" element={<CollectionDetails />} />
+						</Route>
+						<Route path="login">
+							<Route index element={<Login />} />
+						</Route>
 					</Route>
-					<Route path="login">
-						<Route index element={<Login />} />
-					</Route>
-				</Route>
-			</Routes>
-		</Layout>
-	</BrowserRouter>
-)
+				</Routes>
+			</Layout>
+		</BrowserRouter>
+	)
+}
 
 export default AppRoutes
