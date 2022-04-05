383ae7eb735ea1d49fc27542b37e18d2be7df439
John Pittenger <36045366+Gittenger@users.noreply.github.com>
2022-04-04 20:50:24 -0400

    redux persist OK

--- client/src/index.js
+++ client/src/index.js
@@ -3,13 +3,17 @@ import ReactDOM from 'react-dom'
 import App from './App'
 import './styles/main.scss'
 import { Provider as ReduxProvider } from 'react-redux'
+import { PersistGate } from 'redux-persist/integration/react'
+import { persistor } from './redux/store.js'
 import store from './redux/store.js'
 import reportWebVitals from './reportWebVitals'
 
 ReactDOM.render(
 	<React.StrictMode>
 		<ReduxProvider store={store}>
-			<App />
+			<PersistGate persistor={persistor} loading={null}>
+				<App />
+			</PersistGate>
 		</ReduxProvider>
 	</React.StrictMode>,
 	document.getElementById('root')
